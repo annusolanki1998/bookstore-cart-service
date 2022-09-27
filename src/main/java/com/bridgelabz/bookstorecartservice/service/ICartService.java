8ee4d595785331990f9ba553ae5799f3ebad5d95
Annu <annusinghraj1998@gmail.com>
2022-09-27 09:23:20 +0530

    ADDED: Cart service of book store

---
+++ src/main/java/com/bridgelabz/bookstorecartservice/service/ICartService.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.bookstorecartservice.service;
+
+import com.bridgelabz.bookstorecartservice.dto.CartDto;
+import com.bridgelabz.bookstorecartservice.model.CartModel;
+import com.bridgelabz.bookstorecartservice.util.CartResponse;
+
+import java.util.List;
+
+public interface ICartService {
+
+
+    CartResponse updateQuantity(Long cartId, String token, Integer quantity);
+
+    List<CartModel> getAllCartItems(String token);
+
+    CartResponse removingCart(String token, Long cartId);
+
+
+    CartResponse addCart(CartDto cartDto, String token, Long bookId);
+}
